c8f0afd8c082cb1052caff16345bbc16fd858cbd
randomguy1289 <ashutoshshenoy@gmail.com>
2020-10-04 17:00:43 +0530

    Macro definition
    Made a macro definition

--- conditional.c
+++ conditional.c
@@ -1,7 +1,7 @@
 // conditional.c by Bill Weinman <http://bw.org/>
 #include <stdio.h>
 #include "conditional.h"
-
+#define NUMBER 100
 int main( int argc, char ** argv ) {
 	printf("Number is %d\n", NUMBER);
 	return 0;
